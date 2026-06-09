a0d7bac9af9657e4d49e13eb1f20dae6e57979a9
SlmAzr <slm.amd15@gmail.com>
2026-06-09 14:22:47 +0200

    fix /notify in ordercontroller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -45,7 +45,7 @@ exports.createOrder = async (req, res) => {
 
     // Appel au micro-service de notification
     try {
-      await axios.post(`${process.env.NOTIFICATION_SERVICE_URL}`, {
+      await axios.post(`${process.env.NOTIFICATION_SERVICE_URL}/notify`, {
         to: email,
         subject: 'Nouvelle Commande Créée',
         text: `Une commande a été créée avec succès pour les produits suivants : \n${orderDetails
